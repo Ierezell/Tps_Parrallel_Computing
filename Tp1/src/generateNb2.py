d510a8e8916f2163f2c00381a3dd57b0fb3e6352
sebMP <seb.marsaa-poey@orange.fr>
2019-02-10 17:17:20 -0500

    intervalles de test

--- Tp1/src/generateNb2.py
+++ Tp1/src/generateNb2.py
@@ -1,9 +1,12 @@
+#crée des intervalles de tests.
+#le fichier nombresN.txt contient 100 intervalles de largeur 10**N
 import random
 
-with open('./nombres.txt', 'w') as fichier_nombre:
-    fichier_nombre.write("")
-for _ in range(50):  #nombre d'intervalles
-    intervalle_bas = random.randint(1,9) * 10**(random.randint(10,100))
-    intervalle_haut = intervalle_bas + 10**6    #largeur des intervalles
-    with open('./nombres.txt', 'a') as fichier_nombre:
-        fichier_nombre.write(str(intervalle_bas)+" "+str(intervalle_haut)+"\n")
+for j in range(2,11):
+    with open("./nombres"+str(j)+".txt", 'w') as fichier_nombre:
+        fichier_nombre.write("")
+    for _ in range(100):  #nombre d'intervalles
+        intervalle_bas = random.randint(1,9) * 10**(random.randint(10,100))
+        intervalle_haut = intervalle_bas + 10**j   #largeur des intervalles
+        with open("./nombres"+str(j)+".txt", 'a') as fichier_nombre:
+            fichier_nombre.write(str(intervalle_bas)+" "+str(intervalle_haut)+"\n")
