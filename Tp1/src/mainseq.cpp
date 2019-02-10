fdb3dd4d0a3bb25e5df2b2dfadd9579f40d5850b
sebMP <seb.marsaa-poey@orange.fr>
2019-02-10 17:44:52 -0500

    version séquentielle opérationnelle

--- Tp1/src/mainseq.cpp
+++ Tp1/src/mainseq.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <pthread.h>
 #include <gmpxx.h>
+#include "Types.hpp"
 extern "C"
 {
 }
@@ -48,31 +49,31 @@ int main(int argc, char *argv[])
     mpz_set_str(inc, "1", 10); //inc prend la valeur 1, interprété en base 10
     Chrono chron = Chrono();
     float tic = chron.get();
+    float tac;
     if (prime_nb_file.is_open())
     {
-        vector<mpz_class> nb_prime_nb;
+        vector<Custom_mpz_t> nb_prime_nb;
         while (getline(prime_nb_file, line))
         {
             gmp_sscanf(line.c_str(), "%Zd %Zd", interbas, interhaut); //lis les intervalles haut et bas d'une ligne
             mpz_set(nb, interbas);                                    //nb prend la valeur de interbas
             while (mpz_cmp(nb, interhaut) < 0)
             {
-                int is_prime = mpz_probab_prime_p(nb, 50); //determine if nb is prime. probability of error < 4^(-50)
+                int is_prime = mpz_probab_prime_p(nb, 20); //determine if nb is prime. probability of error < 4^(-50)
                 if (is_prime == 1 || is_prime == 2)        //number is certainly prime or probably prime
                 {
-                    nb_prime_nb.push_back((mpz_class)nb);
+                    Custom_mpz_t tmp = Custom_mpz_t(nb);
+                    nb_prime_nb.push_back(tmp);
                 }
                 mpz_add(nb, nb, inc); //nb = nb + 1
             }
-            for (auto i : nb_prime_nb)
-                cout << i << ' ';
-            cout << endl;
         }
-        //sort(nb_prime_nb.begin(), nb_prime_nb.end(), mpz_cmp2); //sort vector nb_prime_nb.   Not functionnal yet
+        sort(nb_prime_nb.begin(), nb_prime_nb.end()); //sort vector nb_prime_nb.   Not functionnal yet
         //print every element in vector nb_prime_nb
+        tac = chron.get();
         for (auto i : nb_prime_nb)
         {
-            cout << i << ' ';
+            cout << i.value << ' ';
         }
 
         mpz_clears(interbas, interhaut, NULL); //free the space used by variables
@@ -80,10 +81,10 @@ int main(int argc, char *argv[])
     }
     else
     {
-        cout << "Impossible d'ouvrir le fichier.\n";
+        cerr << "Impossible d'ouvrir le fichier.\n";
         return EXIT_FAILURE;
     }
-    float tac = chron.get();
+    
     cerr << tac - tic << " secondes" << endl;
 }
 // Il y a plusieurs petites erreurs dans votre code.
