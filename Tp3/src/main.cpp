a90177dd79dcdc2b88ca16665e285bccbf4c7768
Pierre Snell <pierre.snell.1@ulaval.ca>
2019-03-25 17:27:28 -0400

    erdgtjk

--- Tp3/src/main.cpp
+++ Tp3/src/main.cpp
@@ -10,7 +10,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <mpi.h>
-#include <unistd.h>
+#include "Chrono.hpp" // Classe chronomètre pour le temps d'éxécution
 
 using namespace std;
 
@@ -92,28 +92,41 @@ void invertParallel(Matrix &matrice)
     assert(matrice.rows() == matrice.cols());
     // Concatener la matrice à la matrice Identitée [ A I ]
     MatrixConcatCols matrice_et_id(matrice, MatrixIdentity(matrice.rows()));
+    cout << matrice_et_id.str() << endl
+         << endl;
 
     // Debut de l'algorithme
     for (size_t idx_ligne = 0; idx_ligne < matrice.rows(); ++idx_ligne)
     {
         // Répartit les indices des lignes dans les processeurs grâce à un map (dictionnaire)
-        map<double, int> subMatrice_Col_Values;
-        for (size_t idx_ligne_rest = idx_ligne; idx_ligne_rest < matrice.rows(); ++idx_ligne_rest)
+        map<double, int> dic_Values_col;
+        for (size_t idx_ligne_rest = 0; idx_ligne_rest < matrice.rows(); ++idx_ligne_rest)
             if (world_rank == (idx_ligne_rest % world_size))
-                subMatrice_Col_Values.insert(pair<double, int>(matrice(idx_ligne_rest, idx_ligne), idx_ligne_rest));
-            else
-                subMatrice_Col_Values.insert(pair<double, int>(-1000000000000000000, 0));
+                dic_Values_col.insert(pair<double, int>(matrice_et_id(idx_ligne_rest, idx_ligne), idx_ligne_rest));
+        // else if ((matrice.rows() - idx_ligne) < world_size)
+        //     dic_Values_col.insert(pair<double, int>(-1000000, 0));
 
-        // Structure pour avoir l'index et la valeur du pivot
         struct ind_and_val
         {
             double value;
             int index;
         } pivot;
+        // vector<double> vals_pivot;
+        // for (size_t idx_ligne_rest = idx_ligne; idx_ligne_rest < matrice.rows(); ++idx_ligne_rest)
+        // {
+        //     if ((idx_ligne_rest % world_size) == world_rank)
+        //         vals_pivot.push_back(matrice(idx_ligne_rest, idx_ligne));
+        // }
+        // // Structure pour avoir l'index et la valeur du pivot
+        // vector<double>::iterator iter_max = max_element(vals_pivot.begin(), vals_pivot.end());
+        // pivot.value = *iter_max;
+        // pivot.index = distance(vals_pivot.begin(), iter_max);
 
         // Chaque processus ayant ses lignes dans un map celles-ci sont ordonées.
         // On réduit donc sur le max de chaque process qui est le 1er element de son dictionnnaire
-        MPI::COMM_WORLD.Allreduce(&subMatrice_Col_Values.rbegin()->first, &pivot.value, 1, MPI_DOUBLE_INT, MPI_MAXLOC);
+        MPI::COMM_WORLD.Allreduce(&dic_Values_col.rbegin()->first, &pivot, 1, MPI_DOUBLE_INT, MPI_MAXLOC);
+        //MPI::COMM_WORLD.Allreduce(&pivot.value, &pivot.value, 1, MPI_DOUBLE_INT, MPI_MAXLOC);
+        cout << "PIVOT : Valeur : " << pivot.value << "   Index  : " << pivot.index << endl;
         // Il est trop difficile de réduire des structures compliquées alors on réduit et partage (allreduce) la valeur.
         // Celui qui possède le pivot avec la valeur réduite broadcast donc son index a tout les autres.
         // if (world_rank == pivot.index)
@@ -121,58 +134,79 @@ void invertParallel(Matrix &matrice)
         // EN FAIT LE REDUCE PREND AUSSI L'INDEX MAIS JE SAIS PAS POURQUOI :O ptet que c'est lié au dico...
 
         //Vérifier que la matrice n'est pas singulière
-        if (matrice(pivot.index, idx_ligne) == 0)
-            throw runtime_error("Matrix not invertible");
+        if (world_rank == idx_ligne)
+            if (matrice(pivot.index, idx_ligne) == 0)
+                throw runtime_error("Matrix not invertible");
 
         // Échanger la ligne courante avec celle du pivot
         if (pivot.index != idx_ligne)
         {
             // Si l'indice de la ligne et celui du pivot appartiennent au meme processeur, operation immediate...
             if ((idx_ligne % world_size == pivot.index % world_size) && (pivot.index % world_size == world_rank))
+            {
                 matrice_et_id.swapRows(pivot.index, idx_ligne);
+                for (size_t j = 0; j < matrice_et_id.cols(); ++j)
+                    matrice_et_id(idx_ligne, j) /= pivot.value;
+            }
             // ... sinon, communication point à point entre les 2 processus concernés
             // A ESSAYER AVEC UN SENDRECV_REPLACE
             else
             {
                 if ((idx_ligne % world_size) == world_rank)
                 {
-                    valarray<double> ligne_pas_pivot(matrice_et_id.getRowCopy(idx_ligne));
-                    valarray<double> ligne_pivot(ligne_pas_pivot);
+                    valarray<double> ligne_pas_pivot(matrice_et_id.getRowSlice(idx_ligne));
+                    valarray<double> ligne_pivot_norm(ligne_pas_pivot);
                     MPI::COMM_WORLD.Send((void *)&ligne_pas_pivot[0], ligne_pas_pivot.size(), MPI::DOUBLE, pivot.index % world_size, 42);
-                    MPI::COMM_WORLD.Recv((void *)&ligne_pivot[0], ligne_pivot.size(), MPI::DOUBLE, pivot.index % world_size, 666);
-                    matrice_et_id.getRowSlice(idx_ligne) = ligne_pivot;
+                    MPI::COMM_WORLD.Recv((void *)&ligne_pivot_norm[0], ligne_pivot_norm.size(), MPI::DOUBLE, pivot.index % world_size, 666);
+                    matrice_et_id.getRowSlice(idx_ligne) = ligne_pivot_norm;
+                    matrice_et_id.getRowSlice(pivot.index) = ligne_pas_pivot;
                 }
                 else if ((pivot.index % world_size) == world_rank)
                 {
-                    valarray<double> ligne_pivot(matrice_et_id.getRowCopy(pivot.index));
-                    valarray<double> ligne_pas_pivot(ligne_pivot);
+                    for (size_t j = 0; j < matrice_et_id.cols(); ++j)
+                        matrice_et_id(pivot.index, j) /= pivot.value;
+                    valarray<double> ligne_pivot_norm(matrice_et_id.getRowSlice(pivot.index));
+                    valarray<double> ligne_pas_pivot(ligne_pivot_norm);
                     MPI::COMM_WORLD.Recv((void *)&ligne_pas_pivot[0], ligne_pas_pivot.size(), MPI::DOUBLE, idx_ligne % world_size, 42);
-                    MPI::COMM_WORLD.Send((void *)&ligne_pivot[0], ligne_pivot.size(), MPI_DOUBLE, idx_ligne % world_size, 666);
+                    MPI::COMM_WORLD.Send((void *)&ligne_pivot_norm[0], ligne_pivot_norm.size(), MPI_DOUBLE, idx_ligne % world_size, 666);
                     matrice_et_id.getRowSlice(pivot.index) = ligne_pas_pivot;
+                    matrice_et_id.getRowSlice(idx_ligne) = ligne_pivot_norm;
                 }
             }
         }
-        // Broadcast de la ligne idx_ligne à tous les processeurs... (soit la ligne du pivot)
-        valarray<double> ligne_pivot_norm(matrice_et_id.getRowCopy(idx_ligne) / pivot.value);
-        MPI::COMM_WORLD.Bcast((void *)&ligne_pivot_norm[0], ligne_pivot_norm.size(), MPI_DOUBLE, idx_ligne % world_size);
-        matrice_et_id.getRowSlice(idx_ligne) = ligne_pivot_norm;
+        else
+        {
+            cout << "WHAAAAAAAAAAAAA" << endl;
+            if ((pivot.index % world_size) == world_rank)
+                for (size_t j = 0; j < matrice_et_id.cols(); ++j)
+                    matrice_et_id(idx_ligne, j) /= pivot.value;
+        }
+        // Broadcast de la ligne idx_ligne à tous les autres processeurs... (soit la ligne du pivot)
+        valarray<double> ligne_pivot_broadcast(matrice_et_id.getRowSlice(idx_ligne));
+        MPI::COMM_WORLD.Bcast((void *)&ligne_pivot_broadcast[0], ligne_pivot_broadcast.size(), MPI_DOUBLE, idx_ligne % world_size);
+        //matrice_et_id.getRowSlice(idx_ligne) = ligne_pivot_broadcast;
 
         // Fait les opérations de gauss sur les lignes restantes
         for (size_t idx_ligne_rest = 0; idx_ligne_rest < matrice_et_id.rows(); idx_ligne_rest++)
         {
-            valarray<double> ligne_modified(matrice_et_id.getRowSlice(idx_ligne_rest));
             if (((idx_ligne_rest % world_size) == world_rank) && (idx_ligne_rest != idx_ligne))
             {
                 //...qui calculent le facteur d'élimination l(ik) = A(ik)/A(kk)...  (equation 7.2)
-                double fact_elim = matrice_et_id(idx_ligne_rest, idx_ligne);
                 //...et effectuent l'élimination A(ij) = A(ij)-l(ik)A(kj)  (equation 7.3)
                 // valarray<double> array_fact_elim(fact_elim, matrice_et_id.rows());
-                ligne_modified -= ligne_pivot_norm * fact_elim;
+                for (size_t j = 0; j < matrice_et_id.cols(); ++j)
+                    matrice_et_id(idx_ligne_rest, j) -= matrice_et_id(idx_ligne_rest, idx_ligne) * ligne_pivot_broadcast[j];
             }
-            // Puis broadcast pour que tout les processus aient une matrice à jour
-            MPI::COMM_WORLD.Bcast((void *)&ligne_modified[0], ligne_modified.size(), MPI_DOUBLE, idx_ligne_rest % world_size);
-            matrice_et_id.getRowSlice(idx_ligne_rest) = ligne_modified;
         }
+        cout << matrice_et_id.str() << endl
+             << endl;
+        // Puis broadcast pour que tout les processus aient une matrice à jour
+        // for (size_t idx_ligne_rest = 0; idx_ligne_rest < matrice_et_id.rows(); idx_ligne_rest++)
+        // {
+        //     valarray<double> ligne_modified = matrice_et_id.getRowSlice(idx_ligne_rest);
+        //     MPI::COMM_WORLD.Bcast((void *)&ligne_modified[0], ligne_modified.size(), MPI_DOUBLE, idx_ligne_rest % world_size);
+        //     matrice_et_id.getRowSlice(idx_ligne_rest) = ligne_modified;
+        // }
     }
     MPI::Finalize();
     // On copie la partie droite de la matrice AI ainsi transformée dans la matrice courante.
@@ -218,32 +252,39 @@ int main(int argc, char **argv)
     //  << matrice.str() << endl;
 
     Matrix mat_Inv_Seq(matrice);
+    Chrono chron = Chrono();
+    float tic_seq = chron.get();
     invertSequential(mat_Inv_Seq);
-    cout << "Matrice inverse sequentielle:\n"
-         << mat_Inv_Seq.str() << endl
-         << endl;
+    float tac_seq = chron.get();
+    // cout << "Matrice inverse sequentielle:\n"
+    //      << mat_Inv_Seq.str() << endl
+    //      << endl;
 
     Matrix res_Seq = multiplyMatrix(matrice, mat_Inv_Seq);
-    cout << "Produit des deux matrices sequentielle:\n"
-         << res_Seq.str() << endl
-         << endl;
+    // cout << "Produit des deux matrices sequentielle:\n"
+    //      << res_Seq.str() << endl
+    //      << endl;
 
     cout << "Erreur sequentielle: " << res_Seq.getDataArray().sum() - taille_mat << endl
          << endl;
+    cout << "Temps sequentiel : " << tac_seq - tic_seq << "secondes" << endl;
 
     Matrix mat_Inv_Par(matrice);
+    float tic_par = chron.get();
     invertParallel(mat_Inv_Par);
-    cout << "Matrice inverse parallele:\n"
-         << mat_Inv_Par.str() << endl
-         << endl;
+    float tac_par = chron.get();
+    // cout << "Matrice inverse parallele:\n"
+    //      << mat_Inv_Par.str() << endl
+    //      << endl;
 
     Matrix res_Par = multiplyMatrix(matrice, mat_Inv_Par);
-    cout << "Produit des deux matrices parallele:\n"
-         << res_Par.str() << endl
-         << endl;
+    // cout << "Produit des deux matrices parallele:\n"
+    //      << res_Par.str() << endl
+    //      << endl;
 
     cout << "Erreur parallèle: " << res_Par.getDataArray().sum() - taille_mat << endl
          << endl;
+    cout << "Temps parallele : " << tac_par - tic_par << "secondes" << endl;
     return 0;
 }
 
